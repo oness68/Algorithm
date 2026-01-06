14bdc01855498b23250cf428fe61ccc79705c5fc
조진서 <oness68@naver.com>
2026-01-06 19:55:16 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+int main()
+{
+	int X, Y;
+	while (cin >> X >> Y)
+	{
+		if (X == 0 && Y == 0)
+		{
+			break;
+		}
+		cout << X + Y << endl;
+		
+	}
+	
+
+}
