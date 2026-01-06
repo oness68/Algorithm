2a8dbd8a4852d78a625f0b4ed8fdc321ed21cac1
조진서 <oness68@naver.com>
2026-01-06 20:54:19 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+int main()
+{
+	
+	cout << "         ,r'\"7" << endl;
+	cout << "r`-_   ,'  ,/" << endl;
+	cout << " \\. \". L_r'" << endl;
+	cout << "   `~\\/" << endl;
+	cout << "      |" << endl;
+	cout << "      |" << endl;
+
+}
