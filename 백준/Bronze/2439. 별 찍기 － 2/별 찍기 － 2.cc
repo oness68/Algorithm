b04f187008cbad3bc5b3c4685d06af457cf95bb5
조진서 <oness68@naver.com>
2026-01-08 01:21:09 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+
+int main()
+{
+	int N;
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = N-1; j >= 0; j--)
+		{
+			if (j <= i)
+			{
+				cout << "*";
+			}
+			else
+			{
+				cout << " ";
+			}
+
+		}
+		cout << endl;
+	}
+
+}
