c02045717504984c90abb6b29567fc0f4d98b7c3
조진서 <oness68@naver.com>
2026-01-31 02:51:51 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -5,28 +5,34 @@
 #include<algorithm>
 using namespace std;
 
+long long MOD = 1234567891;
 
 int main()
 {
 	int length;
 	string str;
-	int hash = 0;
+	long long hash = 0;
+	long long r=1;
 	cin >> length;
 	cin >> str;
 
-	vector<int> v;
+	vector<long long> v;
 
 	for (int i = 0; i < length; i++)
 	{
-		v.push_back(str[i] - 96);
+		v.push_back((long long)str[i] - 96);
 	}
 
 	
 	for (int i = 0; i < length; i++)
 	{
-		hash += v[i] * pow(31, i);
+		hash += (v[i] * r)%MOD;
+		r *= 31;
+		r %= MOD;
+
+	
 	}
 
-	cout << hash;
+	cout << hash%MOD;
 
 }
